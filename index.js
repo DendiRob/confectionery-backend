9f49ee56494103fd746147edfee22a848efe6397
SavaOurl <dendiroblek@gmail.com>
2023-07-27 14:46:43 +0400

    preparing to work

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json())
+
+app.get('/', (req,res) => {
+    res.send('Hello everyone')
+});
+
+app.listen(4444, (err) => {
+    if(err){
+        return console.log(err)
+    }
+
+    console.log('Server started')
+})
