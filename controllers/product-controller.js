1a0b392a6efc3da2552d61aedef681d8386168ae
SavaOurl <dendiroblek@gmail.com>
2023-08-07 15:56:29 +0400

    new routes in product

--- controllers/product-controller.js
+++ controllers/product-controller.js
@@ -15,7 +15,42 @@ const getAllProducts = (req,res) => {
 
         })
 };
+const getSumProducts = (req,res) => {
+    Product
+        .find().count()
+        .then((product) => {
+            res
+                .status(200)
+                .json(product)
+        })
+        .catch((err) => {
+            res
+                .status(500)
+                console.log(err)
+
+        })
+};
+const getSomeProductsForCatalog = (req,res) => {
+
+    const skipProducts = +req.query.skip;
+
+    Product
+        .find().skip(skipProducts).limit(15)
+        .then((product) => {
+            res
+                .status(200)
+                .json(product)
+        })
+        .catch((err) => {
+            res
+                .status(500)
+                console.log(err)
+
+        })
+}
 
 module.exports = {
-    getAllProducts
+    getAllProducts,
+    getSomeProductsForCatalog,
+    getSumProducts,
 }
--- routes/product-routes.js
+++ routes/product-routes.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const {
-    getAllProducts
+    getAllProducts,
+    getSomeProductsForCatalog,
+    getSumProducts
 } = require('../controllers/product-controller')
 
 const router = express.Router();
 
 
 router.get('/products', getAllProducts)
+router.get('/products/sum', getSumProducts)
+router.get('/products/catalog', getSomeProductsForCatalog )
 
 module.exports = router;
