1ef77e073128be73dc2673fd10233fe057dd7bcf
SavaOurl <dendiroblek@gmail.com>
2023-10-29 16:59:26 +0400

    fixed the counter during loading and corrected the redirect from the admin page without a admin role

--- src/dtos/user-dto.ts
+++ src/dtos/user-dto.ts
@@ -3,16 +3,20 @@ import { Document, Types } from "mongoose";
 interface UserDtoTypes extends Document {
     email: string;
     isActivated: boolean;
+    role: string;
+    _id: Types.ObjectId; 
 }
 
 export default class UserDto {
-    email: string;
-    id: Types.ObjectId; 
+    email;
+    id;
     isActivated;
+    role;
 
     constructor(model: UserDtoTypes) {
         this.email = model.email!;
         this.id = model._id; 
         this.isActivated = model.isActivated;
+        this.role = model.role
     }
 }
--- src/models/user.ts
+++ src/models/user.ts
@@ -6,6 +6,7 @@ const UserSchema = new Schema({
     password: {type: String, required: true},
     isActivated: {type: Boolean, default: false},
     activationLink: {type: String},
+    role: {type: String, default: 'user'}
 })
 
 const User = model('User', UserSchema);
